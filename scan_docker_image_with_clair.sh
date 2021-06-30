f8056e37296d429fb2475bbac7a0f3d6efd25ef3
Daniel Andrzejewski <d.andrzejewski@consultant.crif.com>
2021-06-30 18:29:52 +0200

    Updated Dockerfile

--- scan_docker_image_with_clair.sh
+++ scan_docker_image_with_clair.sh
@@ -26,10 +26,16 @@ echo "http://${REGISTRY}/v2/${IMAGE}/manifests/${TAG}"
 
 for blobsum in $(curl -s http://${REGISTRY}/v2/${IMAGE}/manifests/${TAG} | grep blobSum | cut -d'"' -f4); do
 
-  echo $blobsum
+  echo "Indexing blob ${blobsum} ==> {\"Layer\":{\"Name\":\"${blobsum}\",\"Path\":\"http://${REGISTRY}/v2/${IMAGE}/blobs/${blobsum}\",\"Format\":\"Docker\"}}"
 
-  #curl "http://${CLAIR}/v1/layers/${blobsum}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "High'
-  curl "http://${CLAIR}/v1/layers/${blobsum}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "'
+  #Step 4 Now let’s tell Clair to analyze our docker image composed by only one “scannable” layer.
+  curl -s -X POST http://${CLAIR}/v1/layers -d "{\"Layer\":{\"Name\":\"${blobsum}\",\"Path\":\"http://${REGISTRY}/v2/${IMAGE}/blobs/${blobsum}\",\"Format\":\"Docker\"}}"
+
+  # Step 5 get a result of found vulnerabilities in the layer by calling the endpoint:
+  # GET http://centos:6060/v1/layers/sha256:bc9ab73e5b14b9fbd3687a4d8c1f1360533d6ee9ffc3f5ecc6630794b40257b7?features&vulnerabilitie
+  echo "Getting results of found vulnerabilities in the layer ${blobsum}"
+
+  curl -s "http://${CLAIR}/v1/layers/${blobsum}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "'
 
   resp_count=$(curl -s "http://${CLAIR}/v1/layers/${blobsum}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "High' | wc -c)
 
