595e078dbf37f59fc7e3abea1d4b9357d5cfd40d
Daniel Andrzejewski <d.andrzejewski@consultant.crif.com>
2021-06-30 19:23:41 +0200

    Updated Dockerfile

--- scan_docker_image_with_clair.sh
+++ scan_docker_image_with_clair.sh
@@ -15,10 +15,6 @@ IMAGE=$2
 TAG=$3
 CLAIR=$4
 
-#curl http://centos:5000/v2/debian/manifests/9.5
-
-#for blobsum in $(curl -s http://${REGISTRY}/v2/${IMAGE}/manifests/${TAG} | grep blobSum | awk '{print $2}' | sed 's/"//g' | cut -d':' -f2); do
-
 resp=''
 high_vulnerabilities_found=0
 
@@ -40,11 +36,13 @@ for blobsum in $(curl -s http://${REGISTRY}/v2/${IMAGE}/manifests/${TAG} | grep 
   resp_count=$(curl -s "http://${CLAIR}/v1/layers/${blobsum}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "High' | wc -c)
 
   if [ "${resp_count}" -gt 10 ] ; then
-    echo -e "\n================================================================="
-    echo -e "                High vulnerabilities found in ${IMAGE}:${TAG}"
-    echo -e "=================================================================\n"
-
     high_vulnerabilities_found=1
   fi
 
 done
+
+if [ "${high_vulnerabilities_found}" -eq 1 ] ; then
+  echo -e "\n================================================================="
+  echo -e "                High vulnerabilities found in ${IMAGE}:${TAG}"
+  echo -e "=================================================================\n"
+fi
