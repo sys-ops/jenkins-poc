4a76feff501981de5e6a90cb8bfabe05ebd56841
Daniel Andrzejewski <d.andrzejewski@consultant.crif.com>
2021-07-01 13:49:26 +0200

    Updated Dockerfile

--- scan_docker_image_with_clair.sh
+++ scan_docker_image_with_clair.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#@author daniel.andrzejewski
+#@author Daniel Andrzejewski
 
-#set -x
+set -x
 
 if [ $# -ne 4 ]
 then
@@ -15,25 +15,23 @@ IMAGE=$2
 TAG=$3
 CLAIR=$4
 
-resp=''
 high_vulnerabilities_found=0
 
-echo "http://${REGISTRY}/v2/${IMAGE}/manifests/${TAG}"
+manifests="http://${REGISTRY}/v2/${IMAGE}/manifests/${TAG}"
 
-for blobsum in $(curl -s http://${REGISTRY}/v2/${IMAGE}/manifests/${TAG} | grep blobSum | cut -d'"' -f4); do
+for layer in $(curl -s ${manifests} | grep blobSum | cut -d'"' -f4); do
 
-  echo "Indexing blob ${blobsum} ==> {\"Layer\":{\"Name\":\"${blobsum}\",\"Path\":\"http://${REGISTRY}/v2/${IMAGE}/blobs/${blobsum}\",\"Format\":\"Docker\"}}"
+  echo "Indexing layer ${layer} ==> {\"Layer\":{\"Name\":\"${layer}\",\"Path\":\"http://${REGISTRY}/v2/${IMAGE}/blobs/${layer}\",\"Format\":\"Docker\"}}"
 
-  #Step 4 Now let’s tell Clair to analyze our docker image composed by only one “scannable” layer.
-  curl -s -X POST http://${CLAIR}/v1/layers -d "{\"Layer\":{\"Name\":\"${blobsum}\",\"Path\":\"http://${REGISTRY}/v2/${IMAGE}/blobs/${blobsum}\",\"Format\":\"Docker\"}}"
+  # let CLAIR analyze docker image composed by only one "scannable" layer
+  curl -s -X POST http://${CLAIR}/v1/layers -d "{\"Layer\":{\"Name\":\"${layer}\",\"Path\":\"http://${REGISTRY}/v2/${IMAGE}/blobs/${layer}\",\"Format\":\"Docker\"}}"
 
-  # Step 5 get a result of found vulnerabilities in the layer by calling the endpoint:
-  # GET http://centos:6060/v1/layers/sha256:bc9ab73e5b14b9fbd3687a4d8c1f1360533d6ee9ffc3f5ecc6630794b40257b7?features&vulnerabilitie
-  echo "Getting results of found vulnerabilities in the layer ${blobsum}"
+  # get a result of found vulnerabilities in the layer by calling the endpoint http://${CLAIR}/v1/layers/${layer}
+  echo "Getting results of found vulnerabilities in the layer ${layer}"
 
-  curl -s "http://${CLAIR}/v1/layers/${blobsum}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "'
+  curl -s "http://${CLAIR}/v1/layers/${layer}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "'
 
-  resp_count=$(curl -s "http://${CLAIR}/v1/layers/${blobsum}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "High' | wc -c)
+  resp_count=$(curl -s "http://${CLAIR}/v1/layers/${layer}?vulnerabilities" | jq | grep -B 5 -A 17 'Severity": "High' | wc -c)
 
   if [ "${resp_count}" -gt 10 ] ; then
     high_vulnerabilities_found=1
